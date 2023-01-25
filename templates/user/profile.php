8d89a624c59b30200aef68aacf69ef7e1b2eedda
Maxime Corteel <mcorteel@gmail.com>
2023-01-25 16:35:30 +0100

    Do not force email

--- templates/user/profile.php
+++ templates/user/profile.php
@@ -19,7 +19,7 @@
             <div class="row mb-3">
                 <label class="col-md-4 col-form-label text-md-end">Identifiant</label>
                 <div class="col-md-8">
-                    <input type="email" class="form-control" name="email" value="<?= $app['user']['email']; ?>" />
+                    <input type="email" class="form-control" name="text" value="<?= $app['user']['email']; ?>" />
                 </div>
             </div>
             <div class="row mb-3">
