eab5f7fd9d022d4ea5bf773038b2eb510758140f
Maxime Corteel <mcorteel@gmail.com>
2023-01-28 22:10:37 +0100

    Add autofocus to username field

--- templates/login/index.php
+++ templates/login/index.php
@@ -11,7 +11,7 @@
         <div class="row mb-3">
             <label class="col-md-4 col-form-label text-md-end">Identifiant</label>
             <div class="col-md-8">
-                <input type="text" class="form-control" name="email" />
+                <input type="text" class="form-control" name="email" autofocus />
             </div>
         </div>
         <div class="row mb-3">
